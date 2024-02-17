5cfd3eb262f0366e3dac8eea16e929dd3a0a5975
manasaling <lgmanasa@gmail.com>
2024-02-17 04:39:00 -0800

    fixed card styling

--- src/App.tsx
+++ src/App.tsx
@@ -16,19 +16,24 @@ function App() {
               <Link to="/Home">Home</Link>
             </li>
             <li>
-              <Link to="/profile">Profile</Link>
+              <Link to="/concerts">Concerts</Link>
             </li>
             <li>
-              <Link to="/concerts">Concerts</Link>
+              <Link to="/profile">Profile</Link>
             </li>
             {/* Add more navigation links as needed */}
           </ul>
         </nav>
 
+        <h1>Concert Connect</h1>
+       
+       
+         
+
         <Routes>
           <Route path="/about" element={<About />} /> {/* Use the About component */}
-          <Route path="/profile" element={<Profile />} /> {/* Use the About component */}
           <Route path="/concerts" element={<Concerts />} /> {/* Use the About component */}
+          <Route path="/profile" element={<Profile />} /> {/* Use the About component */}
           {/* Add more routes as needed */}
         </Routes>
       </div>
